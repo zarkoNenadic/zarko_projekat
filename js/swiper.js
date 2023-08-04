49181a4126390bfe023bbff057e8b25405a02b17
Zarko Nenadic <nenadiczarko2@gmail.com>
2023-08-04 08:51:02 +0200

    swipper product-section(responsive)

--- js/swiper.js
+++ js/swiper.js
@@ -6,23 +6,34 @@ const media5 = 700;
 
 let number = 5;
 
-
+window.addEventListener("load",()=>{
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: number,
+    spaceBetween: 30,
+    loop: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
 
 window.addEventListener("resize", (e)=>{
 
-  let browserWidth = window.innerWidth;
-
-  if(media1 == browserWidth){
+  let browserWidthChanged = window.innerWidth;
+  
+  number = 5;
+  
+  if(browserWidthChanged > 1600){
     number = 5;
-  }else if(media2 == browserWidth){
+  }else if(browserWidthChanged < 1500 && browserWidthChanged >= 1290){
     number = 4;
-  }else if(media3 == browserWidth){
+  }else if(browserWidthChanged < 1290 && browserWidthChanged >= 900){
     number = 3;
-  }else if(media4 == browserWidth){
+  }else if(browserWidthChanged < 900 && browserWidthChanged >= 700){
     number = 2;
-  }else if(media5 == browserWidth){
+  }else if(browserWidthChanged < 700 && browserWidthChanged >= 350){
     number = 1;
-    console.log(media5 == browserWidth)
   }
 
   var swiper = new Swiper(".mySwiper", {
