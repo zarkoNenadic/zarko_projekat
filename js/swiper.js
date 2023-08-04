a46ccedf2b03de6eaca786ba1d508d25c307d254
Zarko Nenadic <nenadiczarko2@gmail.com>
2023-08-04 09:21:43 +0200

    swipper product-section(responsive)

--- js/swiper.js
+++ js/swiper.js
@@ -33,11 +33,11 @@ window.addEventListener("resize", (e)=>{
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: number,
     spaceBetween: 30,
-    loop: false,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    loop:true,
+   navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   });
 
 });
