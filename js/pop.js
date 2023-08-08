b13097dd7a915fa9febc925cdcd374d97bb76a0f
Zarko Nenadic <nenadiczarko2@gmail.com>
2023-08-08 08:31:23 +0200

    popup

---
+++ js/pop.js
@@ -0,0 +1,24 @@
+function showPopup() {
+    document.querySelector(".popup").style.display = "block";
+  }
+  
+  function wait(delay) {
+    let scrolled = false;
+  
+    function handleScroll() {
+      if (!scrolled) {
+        scrolled = true;
+        setTimeout(showPopup, delay);
+        window.removeEventListener("scroll", handleScroll);
+      }
+    }
+  
+    window.addEventListener("scroll", handleScroll);
+  }
+  
+  wait(4500);
+  
+  document.querySelector("#closePopup").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+  });
+  
